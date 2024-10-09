9f0325bdda5cbb73add48d196ca836273767f283
billuop123 <biplovthapa890@gmail.com>
2024-10-09 12:26:52 +0545

    World-Wise v1

--- src copy/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./SideBar.module.css";
-function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p className={styles.copyright}>&copy; billu</p>
-    </footer>
-  );
-}
-export default Footer;
